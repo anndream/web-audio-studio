937849d84d18c29061f722d539d6419709a44213
diogo0001 <diogo.t0001@gmail.com>
2021-02-07 20:15:12 -0300

    Adicionada nova track e melhorias

--- audioProcessing.js
+++ audioProcessing.js
@@ -1,14 +1,26 @@
+// TODO: 
+// fazer classes
+// classe Vu
+// classe wave
+// classe track extend Vu, wave
+// função reset para vu e play
 
-var playing = false;
-var audioElementsId = 'audio-1'
-var peakMetersId = 'peak-meter-1'
-var audioSource = ['./samples/Storm.mp3']
-
+var totalTracks = 2
+var playing = [false,false];
+var audioElementsId = ['audio-id-1','audio-id-2']
+var peakMetersId = ['peak-meter-id-1','peak-meter-id-2']
+var wavesId = ['wave-id-1','wave-id-2']
+var volumeId = ['volume-id-1','volume-id-2']
+var panId = ['pan-id-1','pan-id-2']
+var wavesurfer = []
+var responsiveWave = []
 
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+document.getElementById(audioElementsId[0]).setAttribute('type','audio/mpeg');
+document.getElementById(audioElementsId[1]).setAttribute('type','audio/mpeg');
 
-var wavesurfer = WaveSurfer.create({
-    container: document.querySelector('#wave-1'),
+wavesurfer[0] = WaveSurfer.create({
+    container: document.querySelector('#wave-id-1'),
     waveColor: 'rgb(96, 170, 130)',
     progressColor: 'grey',
     cursorColor:'white',
@@ -18,81 +30,94 @@ var wavesurfer = WaveSurfer.create({
     height:100,
     backend: 'MediaElement'
 });
-wavesurfer.zoom(Number(0));
 
-document.getElementById(audioElementsId).setAttribute('type','audio/mpeg');
+wavesurfer[1] = WaveSurfer.create({
+    container: document.querySelector('#wave-id-2'),
+    waveColor: 'rgb(96, 170, 130)',
+    progressColor: 'grey',
+    cursorColor:'white',
+    hideScrollbar: true,
+    splitChannels:true,
+    maxCanvasWidth: 200,
+    height:100,
+    backend: 'MediaElement'
+});
 
-var responsiveWave = wavesurfer.util.debounce(function() {
-    wavesurfer.empty();
-    wavesurfer.drawBuffer();
-  }, 20);
+responsiveWave[0] = wavesurfer[0].util.debounce(function() {
+    wavesurfer[0].empty();
+    wavesurfer[0].drawBuffer();
+}, 20);
   
-window.addEventListener('resize', responsiveWave);
+responsiveWave[1] = wavesurfer[1].util.debounce(function() {
+    wavesurfer[1].empty();
+    wavesurfer[1].drawBuffer();
+}, 20);
 
-// functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+wavesurfer[0].zoom(Number(0));
+wavesurfer[1].zoom(Number(0));
+window.addEventListener('resize', responsiveWave[0]);
+window.addEventListener('resize', responsiveWave[1]);
 
-function setAudioSource(elem,src){
+
+// Audio functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function setAudioSource(elem,src,id){
     document.getElementById(elem).setAttribute('src','./samples/'+src);
-    wavesurfer.load(document.getElementById(elem));
+    wavesurfer[0].load(document.getElementById(elem));
     document.getElementById('track-id-1').innerHTML = src
 }
 
 function runVu(meterId, audioId, options, ctx) {
     var meterElement = document.getElementById(meterId);
     var audioElement = document.getElementById(audioId);
-
     var sourceNode = ctx.createMediaElementSource(audioElement);
     sourceNode.connect(ctx.destination);
     var meterNode = webAudioPeakMeter.createMeterNode(sourceNode, ctx);
     webAudioPeakMeter.createMeter(meterElement, meterNode, options);
-
-    // audioElement.addEventListener('play', function () {
-    //     ctx.resume();
-    // });
 }
 
-function playAudio() {
+function playAudio(id) {
     // var myAudio = document.getElementById(audioElementsId);
 
     if(!playing){
         audioCtx.resume(); 
         // myAudio.play();
         playing = true;
-        document.getElementById('play-pause-i').className= "fa fa-pause";
+        document.getElementById('play-pause-id-i').className= "fa fa-pause";
     }
     else{
         // myAudio.pause();
         playing = false;
-        document.getElementById('play-pause-i').className="fa fa-play";
+        document.getElementById('play-pause-id-i').className="fa fa-play";
     } 
-    wavesurfer.playPause();
+    wavesurfer[0].playPause();
 }
 
-function stopAudio() {
+function stopAudio(id) {
     // audioCtx.resume(); 
-    var myAudio = document.getElementById(audioElementsId);
+    // var myAudio = document.getElementById(audioElementsId);
     // myAudio.pause();
     // myAudio.currentTime = 0;
     playing = false;
-    document.getElementById('play-pause-i').className="fa fa-play";
-    wavesurfer.stop();
+    document.getElementById('play-pause-id-i').className="fa fa-play";
+    wavesurfer[0].stop();
 } 
 
-function setVolume(){
-    var value = document.getElementById('volume-1').value;
+function setVolume(id){
+    var value = document.getElementById('volume-id-1').value;
     value = value/100;
-    wavesurfer.setVolume(value);
-    // console.log(value)
+    wavesurfer[0].setVolume(value);
+    getIdNumber(id);
 }
 
-function setPan(){
-    var value = document.getElementById('pan-1').value;
+function setPan(id){
+    var value = document.getElementById('pan-id-1').value;
 }
 
-document.getElementById("wave-1").onscroll = function() {zoom()};
+document.getElementById("wave-id-1").onscroll = function() {zoom()};
 
-function zoom(){
-    wavesurfer.zoom(Number(0));
+function zoom(id){
+    wavesurfer[0].zoom(Number(0));
 }
 
 // Data functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -108,9 +133,8 @@ function getFiles(){
     createTabe(inp.files);
 }
 
-
 function createTabe(data){
-    var table = document.getElementById("tracks-list");
+    var table = document.getElementById("tracks-list-id");
     
     while (table.lastElementChild) {
         table.removeChild(table.lastElementChild);
@@ -128,10 +152,9 @@ function createTabe(data){
           }else{
             var cellText = document.createTextNode(data[i].size);
           }
-
           row.onclick = function(){return function(){
                 var track = this.cells[0].innerHTML;
-                setAudioSource('audio-1',track)
+                setAudioSource('audio-id-1',track)
           }}(row)
 
           cell.appendChild(cellText);
@@ -139,15 +162,18 @@ function createTabe(data){
         }
         table.appendChild(row)
     }
-    setAudioSource('audio-1',data[0].name)
+    setAudioSource('audio-id-1',data[0].name)
 }
 
-// function getTableData(elem){
-    
-    
-// }
+function getIdNumber(elem){
+    var str = elem.id.split('-')
+    var id = str[str.length-1]
+    // console.log(id)
+    return id;
+}
 
 // Initialize ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-runVu(peakMetersId, audioElementsId
-, { audioMeterStandard: 'true-peak' }, audioCtx);
+runVu(peakMetersId[0], audioElementsId[0], { audioMeterStandard: 'true-peak' }, audioCtx);
+runVu(peakMetersId[1], audioElementsId[1], { audioMeterStandard: 'true-peak' }, audioCtx);
+
 
